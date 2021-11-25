33a63e7854ccf40d640a36bdeb47ff9f76ddde29
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 18:08:00 -0300

    Update Q01.java

--- src/atvd2/Q01.java
+++ src/atvd2/Q01.java
@@ -1,1 +1,19 @@
+/*
+Crie um programa que leia um número e que imprima o seu valor ao quadrado. Caso o quadrado desse número seja igual a 0 (zero), o programa deve sair.
+*/
+import java.util.Scanner;
+import java.lang.Math;
 
+public class Q01{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 1, quadrado;
+
+        while (num != 0){
+            System.out.println("Entre com um numero:");
+            num = sc.nextInt();
+            System.out.println("O quadrado do numero eh " +  Math.pow(num,2));
+        }
+        sc.close();
+    }
+}
