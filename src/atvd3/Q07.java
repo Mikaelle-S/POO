d1efbf5e1a09c7a5c4c6fcbda98ec0061e280573
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 18:51:29 -0300

    Update and rename Q7.java to Q07.java

--- src/atvd3/Q07.java
+++ src/atvd3/Q07.java
@@ -1,16 +1,18 @@
+/*
+Construa uma função que receba um nome e retorne o número de vogais.
+*/
 import java.util.Scanner;
 
-public class Atv3q7 {
+public class Q07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Entre com o nome:");
         String nome = sc.nextLine();
-        int check = vogais(nome);
+        int check = vogais(nome.toLowerCase());
         System.out.println("O numero de vogais no nome eh de: " + check);
     }
     public static int vogais (String nome){
         int contarVogais = 0;
-        nome.toLowerCase();
         for (int i = 0; i < nome.length(); i++){
             char c = nome.charAt(i);
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
