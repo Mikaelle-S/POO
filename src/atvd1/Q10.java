f8590af71e24ac02a2b0470284035bc72fee7ab4
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 18:03:23 -0300

    Update Q10.java

--- src/atvd1/Q10.java
+++ src/atvd1/Q10.java
@@ -5,7 +5,7 @@ o programa deve ler a opção desejada, ler dois valores, executar a operação 
 */
 import java.util.Scanner;
 
-public class atv1q10b{
+public class Q10{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("A - Multiplicar\nB - Somar\nC - Subtrair\nD - Dividir");
