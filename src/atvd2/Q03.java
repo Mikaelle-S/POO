5abcb2c314cae0bece36a47039a80b9944e1e3ee
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 18:11:43 -0300

    Create Q03.java

---
+++ src/atvd2/Q03.java
@@ -0,0 +1,32 @@
+/*
+Escreva um programa que solicite um número entre 1 e 4. Caso o número digitado seja diferente, mostre a mensagem "Número inválido". 
+Caso contrário, escreva o número na tela.
+*/
+import java.util.Scanner;
+
+public class atv2q3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com um numero:");
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Numero: 1");
+                break;
+            case 2:
+                System.out.println("Numero: 2");
+                break;
+            case 3:
+                System.out.println("Numero: 3");
+                break;
+            case 4:
+                System.out.println("Numero: 4");
+                break;
+            default:
+                System.out.println("Numero invalido");
+                break;
+        }
+        sc.close();
+    }
+}
