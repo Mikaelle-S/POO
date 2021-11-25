92d14adcaa387871902abc08fbd65c62daa18786
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 17:53:50 -0300

    Update Q06.java

--- src/atvd1/Q06.java
+++ src/atvd1/Q06.java
@@ -3,7 +3,7 @@ Crie um programa para receber um número e indicar se ele é par ou ímpar.
 */
 import java.util.Scanner;
 
-public class atv1q6{
+public class Q06{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
