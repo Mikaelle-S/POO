10b1b2361d9a946d6586395d5481a8207a32c601
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-25 18:15:02 -0300

    Create Q04.java

---
+++ src/atvd2/Q04.java
@@ -0,0 +1,12 @@
+/*
+Escreva um programa que gere a saída : 0, 2, 4, 6, 8, 10, 12, 14.
+*/
+public class Q04{
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<=14;i++){
+            if(i%2==0)
+                System.out.print(i+" " );
+        }
+    }
+}
