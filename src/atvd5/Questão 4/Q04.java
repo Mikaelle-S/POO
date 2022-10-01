6b1d0349caed1324c766854e07249228d483fbc8
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2022-09-30 21:00:38 -0300

    Create Q04.java

---
+++ src/atvd5/Questão 4/Q04.java
@@ -0,0 +1,7 @@
+//Q04 e Q05.
+//Considere que um programa precisa manipular os meses (janeiro a dezembro).Defina uma enumeração para representar esses valores.
+
+public enum EnumMeses{
+    JANEIRO,FEVEREIRO,MARCO,ABRIL,MAIO,JUNHO,
+    JULHO,AGOSTO,SETEMBRO,OUTUBRO,NOVEMBRO,DEZEMBRO;
+}
