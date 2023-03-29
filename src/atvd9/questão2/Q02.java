35fccd8905f2b6d67c9a6be633cc17a45316cee0
Mikaelle <58008533+Mikaelle-S@users.noreply.github.com>
2023-03-29 18:59:40 -0300

    Update Q02.java

--- src/atvd9/questão2/Q02.java
+++ src/atvd9/questão2/Q02.java
@@ -1,1 +1,14 @@
+/* 2. Computador (Classe Composta) Teclado, Monitor, Memória, Placa Mãe (Classes Componentes)*/
+public class Q02 {
+    public static void main(String[] args) {
+        Memoria mr = new Memoria("HyperX", 4);
+        Monitor mn = new Monitor("Samsung", 16);
+        Placa p = new Placa("Asus", "i5");
+        Teclado t = new Teclado("HyperX", "Mecânico");
+
+        Computador computador = new Computador(mr,mn,p,t);
+        System.out.println(computador.toString());
+        
+    }
+}
 
